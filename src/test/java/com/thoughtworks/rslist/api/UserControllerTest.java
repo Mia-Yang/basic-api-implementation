01b7d11c28a7f8483a5dd9f0615d1b7722c5f15b
Yang Siyu <siyuyang1995@gmail.com>
2020-08-06 01:09:56 +0800

    [yang siyu]feat: should check constraints

--- src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
@@ -20,12 +20,52 @@ class UserControllerTest {
 
     @Test
     public void should_register_user() throws Exception {
-        User user = new User("Siyu Yang", "female", 25, "siyu@c.com", "18866688888");
+        User user = new User("SiyuYang", "female", 25, "siyu@c.com", "18866688888");
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonString = objectMapper.writeValueAsString(user);
 
         mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void user_name_should_less_than_8() throws Exception {
+        User user = new User("Siyu-Yang", "female", 25, "siyu@c.com", "18866688888");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void user_age_should_between_18_and_100() throws Exception {
+        User user = new User("SiyuYang", "female", 5, "siyu@c.com", "18866688888");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void email_should_in_correct_format() throws Exception {
+        User user = new User("SiyuYang", "female", 25, "siyuc.com", "18866688888");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void phone_should_in_correct_format() throws Exception {
+        User user = new User("SiyuYang", "female", 25, "siyu@c.com", "18888866688888");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
     }
 
 }
